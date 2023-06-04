ea1bc267c2033a89030b2510dfc9a77200cff120
MANISHA NAYAK <nayakmanisha515@gmail.com>
2023-06-04 18:29:32 +0530

    added header and headerCartButton components

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import classes from "./Header.module.css";
+
+import mealsImage from "../../assets/meals.jpg";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <button>Cart</button>
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="Atable full f delicious food" />
+      </div>
+    </>
+  );
+};
 
-export default Header
+export default Header;
---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HeaderCartButton = () => {
+  return (
+    <div>HeaderCartButton</div>
+  )
+}
+
+export default HeaderCartButton
